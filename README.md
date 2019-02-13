719895f561c652c5667dcf9626a63e397a51824f
K.J. Valencik <kjvalencik@gmail.com>
2019-02-13 11:29:09 -0500

    Fix-up and document `EventEmitter` example.
    - Remove unrelated code
    - Implement receiver timeout
    - Document
      * Design
      * Limitations
      * Code

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 | 15.|  [sharing binary data](https://github.com/neon-bindings/examples/tree/master/sharing-binary-data) | Handling binary data passed from Node to Rust |
 | 16.|  [electron app](https://github.com/neon-bindings/examples/tree/master/electron-app) | A simple electron app using Neon modules |
 | 17.|  [publishing modules](https://github.com/amilajack/disk-utility) | Using [`node-pre-gyp`](https://github.com/mapbox/node-pre-gyp) to build and publish binaries for multiple platforms |
-| 18.|  [event emitter](https://github.com/neon-bindings/examples/tree/master/event-emitter) | An example of creating an `EventEmitter` with Neon for [Express](https://expressjs.com) |
+| 18.|  [event emitter](https://github.com/neon-bindings/examples/tree/master/event-emitter) | An example of creating an `EventEmitter` with Neon |
 | 19.|  Bindgen | Planned |
 
 ## Setup
--- event-emitter/native/src/lib.rs
+++ event-emitter/native/src/lib.rs
@@ -1,218 +1,124 @@
-#[macro_use]
-extern crate neon;
-extern crate neon_serde;
-
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_bytes;
-extern crate serde_json;
-
-use std::sync::mpsc::{self, TryRecvError};
+use std::sync::mpsc::{self, RecvTimeoutError, TryRecvError};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-use neon::context::{Context, FunctionContext, TaskContext};
+use neon::context::{Context, TaskContext};
 use neon::object::Object;
-use neon::result::{JsResult, JsResultExt};
+use neon::result::JsResult;
 use neon::task::Task;
-use neon::types::{JsArray, JsFunction, JsNumber, JsObject, JsString, JsUndefined, JsValue};
+use neon::types::{JsFunction, JsUndefined, JsValue};
+use neon::{class_definition, declare_types, impl_managed, register_module};
 
-use serde_bytes::ByteBuf;
-
-#[derive(Deserialize)]
-struct Request {
-    body: ByteBuf,
-}
-
-#[derive(Deserialize)]
-struct HelloRequest {
-    name: String,
-}
-
-#[derive(Serialize)]
-struct HelloResponse {
-    greeting: String,
-}
-
-fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let arg0 = cx.argument(0)?;
-
-    let req: Request = neon_serde::from_value(&mut cx, arg0)?;
-    let req_body: HelloRequest =
-        serde_json::from_slice(&req.body).or_else(|err| cx.throw_error(&err.to_string()))?;
-
-    let res = HelloResponse {
-        greeting: format!("Hello, {}!", req_body.name),
-    };
-
-    let res_body = serde_json::to_vec(&res).or_else(|err| cx.throw_error(&err.to_string()))?;
-
-    let ret = neon_serde::to_value(&mut cx, &ByteBuf::from(res_body))?;
-
-    Ok(ret)
-}
-
-fn parse(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let s = cx.argument::<JsString>(0)?;
-
-    let o: serde_json::Value =
-        serde_json::from_str(&s.value()).or_else(|err| cx.throw_error(&err.to_string()))?;
-
-    let o = neon_serde::to_value(&mut cx, &o)?;
-
-    Ok(o)
-}
-
-fn stringify(mut cx: FunctionContext) -> JsResult<JsString> {
-    let o = cx.argument(0)?;
-
-    let o: serde_json::Value = neon_serde::from_value(&mut cx, o)?;
-    let s = serde_json::to_string(&o).or_else(|err| cx.throw_error(&err.to_string()))?;
-
-    Ok(JsString::new(&mut cx, &s))
-}
-
-struct SuccessTask;
-
-impl Task for SuccessTask {
-    type Output = i32;
-    type Error = String;
-    type JsEvent = JsNumber;
-
-    fn perform(&self) -> Result<Self::Output, Self::Error> {
-        Ok(17)
-    }
-
-    fn complete(
-        self,
-        mut cx: TaskContext,
-        result: Result<Self::Output, Self::Error>,
-    ) -> JsResult<Self::JsEvent> {
-        Ok(JsNumber::new(&mut cx, result.unwrap() as f64))
-    }
-}
-
-fn perform_async_task(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let f = cx.argument::<JsFunction>(0)?;
-    SuccessTask.schedule(f);
-    Ok(JsUndefined::new())
-}
-
-fn array_process(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let arr = cx.argument::<JsArray>(0)?;
-
-    for i in 0..arr.len() {
-        let item = arr
-            .get(&mut cx, i)?
-            .downcast::<JsObject>()
-            .or_throw(&mut cx)?;
-        let operator = item
-            .get(&mut cx, "operator")?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value();
-        let value = item
-            .get(&mut cx, "value")?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value();
-
-        match operator.as_str() {
-            "print" => {
-                println!("{}", value);
-            }
-            _ => {
-                let msg = format!("Unsupported operator: {}", operator);
-
-                return cx.throw_error(&msg);
-            }
-        }
-    }
-
-    Ok(JsUndefined::new())
-}
-
-#[derive(Deserialize)]
-#[serde(tag = "operator")]
-enum Operation {
-    #[serde(rename = "print")]
-    Print { value: String },
-}
-
-fn array_process_serde(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let arg0 = cx.argument(0)?;
-    let ops: Vec<Operation> = neon_serde::from_value(&mut cx, arg0)?;
-
-    for op in ops {
-        match op {
-            Operation::Print { value } => {
-                println!("{}", value);
-            }
-        }
-    }
-
-    Ok(JsUndefined::new())
-}
-
+/// Represents the data that will be received by the `poll` method. It may
+/// include different types of data or be replaced with a more simple type,
+/// e.g., `Vec<u8>`.
 pub enum Event {
     Tick { count: f64 },
 }
 
-fn event_thread(rx: mpsc::Receiver<()>) -> mpsc::Receiver<Event> {
-    let (tx, events) = mpsc::channel();
+/// Placeholder to represent work being done on a Rust thread. It could be
+/// reading from a socket or any other long running task.
+///
+/// Accepts a shutdown channel `shutdown_rx` as an argument. Allows graceful
+/// for graceful shutdown by reading from this channel. Shutdown may also be
+/// accomplished by waiting for a failed `send` which only occurs when the
+/// receiver has hung-up. However, the shutdown channel pattern allows for
+/// more control.
+///
+/// Returns a `Receiver` channel with the data. This is the channel that will
+/// be read by the `poll` method.
+///
+/// It's also useful to note that the `tx` channel created may be cloned if
+/// there are multiple threads that produce data to be consumed by Neon.
+fn event_thread(shutdown_rx: mpsc::Receiver<()>) -> mpsc::Receiver<Event> {
+    // Create sending and receiving channels for the event data
+    let (tx, events_rx) = mpsc::channel();
 
+    // Spawn a thead to continue running after this method has returned.
     thread::spawn(move || {
         let mut count = 0.0;
 
+        // This loop represents the work being performed
         loop {
             thread::sleep(Duration::from_millis(500));
 
-            match rx.try_recv() {
+            // Check to see if the thread should be shutdown. `try_recv()`
+            // does not block waiting on data.
+            match shutdown_rx.try_recv() {
+                // Shutdown if we have received a command or if there is
+                // nothing to send it.
                 Ok(_) | Err(TryRecvError::Disconnected) => {
                     break;
                 }
+                // No shutdown command, continue
                 Err(TryRecvError::Empty) => {}
             }
 
+            // Send the event data on the channel. Failure may only occur if
+            // there is no receiver. I.e., if the receiver was destroyed
+            // without shutdown. This should only occur if the shutdown
+            // channel outlives the event channel.
             tx.send(Event::Tick { count }).expect("Send failed");
+
             count += 1.0;
         }
     });
 
-    events
+    events_rx
 }
 
-pub struct EventEmitter {
-    events: Arc<Mutex<mpsc::Receiver<Event>>>,
-    shutdown: mpsc::Sender<()>,
-}
-
+/// Reading from a channel `Receiver` is a blocking operation. This struct
+/// wraps the data required to perform a read asynchronously from a libuv
+/// thread.
 pub struct EventEmitterTask(Arc<Mutex<mpsc::Receiver<Event>>>);
 
+/// Implementation of a neon `Task` for `EventEmitterTask`. This task reads
+/// from the events channel and calls a JS callback with the data.
 impl Task for EventEmitterTask {
-    type Output = Event;
+    type Output = Option<Event>;
     type Error = String;
-    type JsEvent = JsObject;
+    type JsEvent = JsValue;
 
+    /// The work performed on the `libuv` thread. First acquire a lock on
+    /// the receiving thread and then return the received data.
+    /// In practice, this should never need to wait for a lock since it
+    /// should only be executed one at a time by the `EventEmitter` class.
     fn perform(&self) -> Result<Self::Output, Self::Error> {
         let rx = self
             .0
             .lock()
             .map_err(|_| "Could not obtain lock on receiver".to_string())?;
 
-        rx.recv().map_err(|_| "Failed to receive event".to_string())
+        // Attempt to read from the channel. Block for at most 100 ms.
+        match rx.recv_timeout(Duration::from_millis(100)) {
+            Ok(event) => Ok(Some(event)),
+            Err(RecvTimeoutError::Timeout) => Ok(None),
+            Err(RecvTimeoutError::Disconnected) => Err("Failed to receive event".to_string()),
+        }
     }
 
+    /// After the `perform` method has returned, the `complete` method is
+    /// scheduled on the main thread. It is responsible for converting the
+    /// Rust data structure into a JS object.
     fn complete(
         self,
         mut cx: TaskContext,
         event: Result<Self::Output, Self::Error>,
     ) -> JsResult<Self::JsEvent> {
+        // Receive the event or return early with the error
         let event = event.or_else(|err| cx.throw_error(&err.to_string()))?;
 
+        // Timeout occured, return early with `undefined
+        let event = match event {
+            Some(event) => event,
+            None => return Ok(JsUndefined::new().upcast()),
+        };
+
+        // Create an empty object `{}`
         let o = cx.empty_object();
 
+        // Creates an object of the shape `{ "event": string, ...data }`
         match event {
             Event::Tick { count } => {
                 let event_name = cx.string("tick");
@@ -223,36 +129,66 @@ impl Task for EventEmitterTask {
             }
         }
 
-        Ok(o)
+        Ok(o.upcast())
     }
 }
 
+/// Rust struct that holds the data required by the `JsEventEmitter` class.
+pub struct EventEmitter {
+    // Since the `Receiver` is sent to a thread and mutated, it must be
+    // `Send + Sync`. Since, correct usage of the `poll` interface should
+    // only have a single concurrent consume, we guard the channel with a
+    // `Mutex`.
+    events: Arc<Mutex<mpsc::Receiver<Event>>>,
+
+    // Channel used to perform a controlled shutdown of the work thread.
+    shutdown: mpsc::Sender<()>,
+}
+
+/// Implementation of the `JsEventEmitter` class. This is the only public
+/// interface of the Rust code. It exposes the `poll` and `shutdown` methods
+/// to JS.
 declare_types! {
     pub class JsEventEmitter for EventEmitter {
+        // Called by the `JsEventEmitter` constructor
         init(_) {
             let (shutdown, shutdown_rx) = mpsc::channel();
+
+            // Start work in a separate thread
             let rx = event_thread(shutdown_rx);
 
+            // Construct a new `EventEmitter` to be wrapped by the class.
             Ok(EventEmitter {
                 events: Arc::new(Mutex::new(rx)),
                 shutdown,
             })
         }
 
+        // This method should be called by JS to receive data. It accepts a
+        // `function (err, data)` style asynchronous callback. It may be called
+        // in a loop, but care should be taken to only call it once at a time.
         method poll(mut cx) {
+            // The callback to be executed when data is available
             let cb = cx.argument::<JsFunction>(0)?;
             let this = cx.this();
+
+            // Create an asynchronously `EventEmitterTask` to receive data
             let events = cx.borrow(&this, |emitter| Arc::clone(&emitter.events));
             let emitter = EventEmitterTask(events);
 
+            // Schedule the task on the `libuv` thread pool
             emitter.schedule(cb);
 
+            // The `poll` method does not return any data.
             Ok(JsUndefined::new().upcast())
         }
 
+        // The shutdown method may be called to stop the Rust thread. It
+        // will error if the thread has already been destroyed.
         method shutdown(mut cx) {
             let this = cx.this();
 
+            // Unwrap the shutdown channel and send a shutdown command
             cx.borrow(&this, |emitter| emitter.shutdown.send(()))
                 .or_else(|err| cx.throw_error(&err.to_string()))?;
 
@@ -261,13 +197,9 @@ declare_types! {
     }
 }
 
+/// Expose the neon objects as a node module
 register_module!(mut cx, {
-    cx.export_function("parse", parse)?;
-    cx.export_function("stringify", stringify)?;
-    cx.export_function("hello", hello)?;
-    cx.export_function("performAsyncTask", perform_async_task)?;
-    cx.export_function("arrayProcess", array_process)?;
-    cx.export_function("arrayProcessSerde", array_process_serde)?;
+    // Expose the `JsEventEmitter` class as `EventEmitter`.
     cx.export_class::<JsEventEmitter>("EventEmitter")?;
 
     Ok(())
