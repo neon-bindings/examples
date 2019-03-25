4c2ea2bad2b678e2ff93346b29cc974f774e36ff
qm3ster <qm3ster@gmail.com>
2019-03-25 21:25:42 +0200

    Fix event-emitter test
    Has been broken since previous refactor

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -12,11 +12,7 @@ const classes = require('../classes');
 const modules = require('../modules');
 const errors = require('../errors');
 const async = require('../async');
-const {
-  parse,
-  stringify,
-  performAsyncTask: performAsyncTaskCB
-} = require('../event-emitter');
+const Emitter = require('../event-emitter/lib/emitter');
 require('../word-counting');
 
 describe('tests', () => {
@@ -98,31 +94,20 @@ describe('tests', () => {
     expect(result).toMatchSnapshot();
   });
 
-  it('should run event emitter', async () => {
-    const performAsyncTask = () =>
-      new Promise(resolve => {
-        performAsyncTaskCB((err, result) => {
-          resolve(result);
-        });
-      });
-
-    const fixture = {
-      a: 1,
-      b: {
-        c: [
-          2,
-          3,
-          {
-            d: '4'
-          }
-        ]
-      }
-    };
-
-    expect(parse(JSON.stringify(fixture))).toMatchSnapshot();
-    expect(JSON.parse(stringify(fixture))).toMatchSnapshot();
-    expect(parse(stringify(fixture))).toMatchSnapshot();
-    const result = await performAsyncTask();
-    expect(result).toEqual(17);
+  it('should run event emitter', () => {
+    const e = new Emitter();
+    const spy = jest.fn();
+    e.on('tick', spy);
+    expect.assertions(2);
+    setTimeout(() => {
+      e.shutdown();
+    }, 1250);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        expect(spy).toHaveBeenCalledTimes(2);
+        expect(spy).toHaveBeenLastCalledWith({ count: 1 });
+        resolve();
+      }, 1750);
+    });
   });
 });
