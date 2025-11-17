dda8646907cc6d12c1641effb70bc4447651cfcd
K.J. Valencik <kjvalencik@gmail.com>
2025-11-17 12:48:49 -0500

    Update cpu-count

--- examples/cpu-count/src/lib.rs
+++ examples/cpu-count/src/lib.rs
@@ -1,11 +1,4 @@
-use neon::prelude::*;
-
-fn get_num_cpus(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    Ok(cx.number(num_cpus::get() as f64))
-}
-
-#[neon::main]
-fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("get", get_num_cpus)?;
-    Ok(())
+#[neon::export]
+fn get_num_cpus() -> f64 {
+    num_cpus::get() as f64
 }
